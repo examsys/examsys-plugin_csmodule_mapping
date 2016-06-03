2cfb63aa897dbdfaa2c9c1abb6e4846e4f355a9b
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2016-06-03 15:55:13 +0100

    corrected install defaults

--- db/install.sql
+++ db/install.sql
@@ -1,8 +1,8 @@
 INSERT IGNORE INTO config (component, setting, value) values ('plugin_csmodule_mapping', 'isconnectedquery', 'N');
 INSERT IGNORE INTO config (component, setting, value) values ('plugin_csmodule_mapping', 'maxrows', 1);
-INSERT IGNORE INTO config (component, setting, value) values ('plugin_csmodule_mapping', 'prompt_uniquepromptname', 'CAMPUS_MODUL_CD');
-INSERT IGNORE INTO config (component, setting, value) values ('plugin_csmodule_mapping', 'filterfields', 'A.UN_SATURN_MODUL_CD,A.UN_CAMPUS_MODUL_CD');
-INSERT IGNORE INTO config (component, setting, value) values ('plugin_csmodule_mapping', 'url', 'https://csdev.transform.nottingham.ac.uk/PSIGW/RESTListeningConnector/CSDEV/ExecuteQuery.v1/PUBLIC/UN_CAMPUS_TO_SATURN_MOD_MAP/WEBROWSET/NONFILE?');
+INSERT IGNORE INTO config (component, setting, value) values ('plugin_csmodule_mapping', 'prompt_uniquepromptname', 'promptname');
+INSERT IGNORE INTO config (component, setting, value) values ('plugin_csmodule_mapping', 'filterfields', 'filterfields');
+INSERT IGNORE INTO config (component, setting, value) values ('plugin_csmodule_mapping', 'url', 'https://www.example.com');
 INSERT IGNORE INTO config (component, setting, value) values ('plugin_csmodule_mapping', 'username', 'username');
 INSERT IGNORE INTO config (component, setting, value) values ('plugin_csmodule_mapping', 'password', 'password');
 INSERT IGNORE INTO config (component, setting, value) values ('plugin_csmodule_mapping', 'timeout', 10);
