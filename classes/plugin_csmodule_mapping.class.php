cb9b25318a3dcea087607a734b51c84a64ee7296
Joseph Baxter <joseph.baxter@nottingham.ac.uk>
2016-08-05 14:46:25 +0100

    decrypt done in config class

--- classes/plugin_csmodule_mapping.class.php
+++ classes/plugin_csmodule_mapping.class.php
@@ -57,8 +57,7 @@ class plugin_csmodule_mapping extends \plugins\plugins_mapping {
         // Strip last &.
         $url = substr($url, 0, -1);
         $username = $this->config->get_setting($this->plugin, 'username');
-        $encryp = new \encryp();
-        $password = $encryp->mdecrypt_password($this->config->get_setting($this->plugin, 'password'));
+        $password = $this->config->get_setting($this->plugin, 'password');
         $timeout = $this->config->get_setting($this->plugin, 'timeout');
         $options = array(CURLOPT_TIMEOUT => $timeout,
             CURLOPT_SSL_VERIFYPEER => $this->config->get_setting($this->plugin, 'ssl_verify')
